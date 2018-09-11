ae663cdf2b017f56810f3641ab5f413329c77b4e
fran <franciscogonmir@gmail.com>
2018-09-12 00:06:19 +0200

    Comentario de clienteDao

--- src/main/java/com/spring/jpa/dao/IClienteDao.java
+++ src/main/java/com/spring/jpa/dao/IClienteDao.java
@@ -3,10 +3,30 @@ import java.util.List;
 
 import com.spring.jpa.models.entity.*;
 public interface IClienteDao {
-
+/**
+ * devuelve una lista de todos los clientes
+ * @return lista de clientes
+ */
 	public List<Cliente> findAll();
+	/**
+	 * inserat un cliente
+	 * @param cliente cliente a insertar
+	 */
 	public void save(Cliente cliente);
+	/**
+	 * obtiene un cliente
+	 * @param id el identificador del cliente
+	 * @return un cliente
+	 */
 	public Cliente findOne(Long id);
+	/**
+	 * actualiza un cliente
+	 * @param cliente cliente a actualizar
+	 */
 	public void update(Cliente cliente);
+	/**
+	 * borra un cliente
+	 * @param id del cliente ha borrar
+	 */
 	public void remove(Long id);
 }
